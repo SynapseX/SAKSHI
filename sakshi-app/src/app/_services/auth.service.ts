51bf3982af01ec9903cb416a83eabfc2e254e299
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2025-03-23 00:40:19 +0530

    auth:redirect

--- sakshi-app/src/app/_services/auth.service.ts
+++ sakshi-app/src/app/_services/auth.service.ts
@@ -2,94 +2,92 @@ import { Injectable } from '@angular/core';
 import { type FirebaseApp, initializeApp } from 'firebase/app';
 import { type Analytics, getAnalytics } from 'firebase/analytics';
 import {
-    GoogleAuthProvider,
-    getAuth,
-    signInWithPopup,
-    setPersistence,
-    onAuthStateChanged,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  setPersistence,
+  onAuthStateChanged,
 } from 'firebase/auth';
 import { BehaviorSubject } from 'rxjs';
 import { User } from '../_models/User';
 import { ConfigService } from './config.service';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class AuthService {
-    #app!: FirebaseApp;
-    #analytics!: Analytics;
+  #app!: FirebaseApp;
+  #analytics!: Analytics;
 
-    private currentUserSource = new BehaviorSubject<User | null>(null);
-    currentUser$ = this.currentUserSource.asObservable();
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
-    constructor(private cfgSrv: ConfigService) {
-        const firebaseConfig = {
-            apiKey: cfgSrv.get('API_KEY'),
-            authDomain: cfgSrv.get('AUTH_DOMAIN'),
-            projectId: cfgSrv.get('PROJECT_ID'),
-            storageBucket: cfgSrv.get('STORAGE_BUCKET'),
-            messagingSenderId: cfgSrv.get('MESSAGING_SENDER_ID'),
-            appId: cfgSrv.get('APP_ID'),
-            measurementId: cfgSrv.get('MEASUREMENT_ID'),
-        };
-        if (!this.#app) this.#app = initializeApp(firebaseConfig);
-        if (!this.#analytics) this.#analytics = getAnalytics(this.#app);
-    }
+  constructor(private cfgSrv: ConfigService) {
+    const firebaseConfig = {
+      apiKey: cfgSrv.get('API_KEY'),
+      authDomain: cfgSrv.get('AUTH_DOMAIN'),
+      projectId: cfgSrv.get('PROJECT_ID'),
+      storageBucket: cfgSrv.get('STORAGE_BUCKET'),
+      messagingSenderId: cfgSrv.get('MESSAGING_SENDER_ID'),
+      appId: cfgSrv.get('APP_ID'),
+      measurementId: cfgSrv.get('MEASUREMENT_ID'),
+    };
+    if (!this.#app) this.#app = initializeApp(firebaseConfig);
+    if (!this.#analytics) this.#analytics = getAnalytics(this.#app);
+  }
 
-    checkUserLoggedIn() {
-        const auth = getAuth();
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                user.getIdToken().then((token) => {
-                    const userObj: User = {
-                        displayName: user.displayName,
-                        email: user.email,
-                        photoURL: user.photoURL,
-                        uid: user.uid,
-                        accessToken: token,
-                    };
-                    this.currentUserSource.next(userObj);
-                });
-            } else {
-                this.currentUserSource.next(null);
-            }
+  checkUserLoggedIn() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        user.getIdToken().then((token) => {
+          const userObj: User = {
+            displayName: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+            uid: user.uid,
+            accessToken: token,
+          };
+          this.currentUserSource.next(userObj);
         });
-    }
+      } else {
+        this.currentUserSource.next(null);
+      }
+    });
+  }
 
-    googleSignIn() {
-        signInWithPopup(getAuth(this.#app), new GoogleAuthProvider())
-            .then((result) => {
-                const credential =
-                    GoogleAuthProvider.credentialFromResult(result);
-                const token = credential?.accessToken;
-                const user: User = {
-                    displayName: result.user.displayName,
-                    email: result.user.email,
-                    photoURL: result.user.photoURL,
-                    uid: result.user.uid,
-                    accessToken: token,
-                };
-                this.currentUserSource.next(user);
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                const email = error.customData.email;
-                const credential =
-                    GoogleAuthProvider.credentialFromError(error);
+  googleSignIn() {
+    signInWithPopup(getAuth(this.#app), new GoogleAuthProvider())
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user: User = {
+          displayName: result.user.displayName,
+          email: result.user.email,
+          photoURL: result.user.photoURL,
+          uid: result.user.uid,
+          accessToken: token,
+        };
+        this.currentUserSource.next(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        // const credential = GoogleAuthProvider.credentialFromError(error);
 
-                console.error({ errorCode, errorMessage, email });
-            });
-    }
+        console.error({ errorCode, errorMessage, email });
+      });
+  }
 
-    googleSignOut() {
-        getAuth(this.#app)
-            .signOut()
-            .then(() => {
-                this.currentUserSource.next(null);
-            })
-            .catch((error) => {
-                console.log('Error in signout', error);
-            });
-    }
+  googleSignOut() {
+    getAuth(this.#app)
+      .signOut()
+      .then(() => {
+        this.currentUserSource.next(null);
+      })
+      .catch((error) => {
+        console.log('Error in signout', error);
+      });
+  }
 }
--- sakshi-app/src/app/auth/auth.component.ts
+++ sakshi-app/src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { Subscription } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -12,11 +13,14 @@ import { ToastrService } from 'ngx-toastr';
 export class AuthComponent implements OnInit, OnDestroy {
   signinSub!: Subscription;
 
-  constructor(private authSrv: AuthService) {}
+  constructor(private authSrv: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.signinSub = this.authSrv.currentUser$.subscribe((user) => {
-      console.log(user);
+      console.log(user, this.router.url);
+      if (user && this.router.url == '/auth') {
+        this.router.navigate(['/']);
+      }
     });
   }
 
