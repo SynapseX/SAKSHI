7543b94317de2d26529000524b9d5ad57ca503eb
Saroj Kumar Sahoo <dev.sarojkumar18@gmail.com>
2025-06-07 12:21:28 +0530

    session fixes

--- sakshi-app/src/app/app.component.ts
+++ sakshi-app/src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
-import {TextToSpeechService} from "@/speech-module/text-to-speech.service";
+import { TextToSpeechService } from '@/speech-module/text-to-speech.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, NavComponent, HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'sakshi.ai';
--- sakshi-app/src/app/features/sessions/create/create.component.ts
+++ sakshi-app/src/app/features/sessions/create/create.component.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 
 import { ToastrService } from 'ngx-toastr';
 import { SessionService } from '@/_services/session.service';
@@ -15,24 +16,25 @@ import { ModalComponent } from '@/components/modal/modal.component';
 import { minutes } from '@/_utils';
 import { AuthService } from '@/_services/auth.service';
 
+const MatImports = [
+  MatInputModule,
+  MatSelectModule,
+  MatTooltipModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatIconModule,
+  MatStepperModule,
+];
+
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatInputModule,
-    MatSelectModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    MatIconModule,
-    RouterModule,
-    ModalComponent,
-  ],
+  imports: [...MatImports, ReactiveFormsModule, RouterModule, ModalComponent],
   templateUrl: './create.component.html',
 })
 export class CreateComponent implements OnInit {
-  sessionForm!: FormGroup;
+  step1Form!: FormGroup;
+  step2Form!: FormGroup;
   minutes = minutes;
   modalState = false;
 
@@ -47,31 +49,40 @@ export class CreateComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.sessionForm = this.initSessionForm();
+    [this.step1Form, this.step2Form] = this.initSessionForm();
   }
 
   initSessionForm() {
-    return this.fb.group({
+    const step_1_form = this.fb.group({
       name: [this.authSrv.getUser()?.displayName || '', Validators.required],
       sessionTime: [new Date().toUTCString()],
       sessionDuration: [30, Validators.required],
       treatmentGoals: ['', Validators.required],
       clientExpectations: ['', Validators.required],
+    });
+    const step_2_form = this.fb.group({
       terminationPlan: ['', Validators.required],
       reviewOfProgress: [''],
       thankYouNote: [''],
       sessionNotes: [''],
     });
+
+    return [step_1_form, step_2_form];
   }
 
   submitSessionForm() {
-    if (this.sessionForm.invalid) {
+    if (this.step1Form.invalid || this.step2Form.invalid) {
       this.openModal();
       return;
     }
     this.isLoading = true;
 
-    this.sessSrv.createSession(this.sessionForm.value).subscribe({
+    const sessionFormData = {
+      ...this.step1Form.value,
+      ...this.step2Form.value,
+    };
+
+    this.sessSrv.createSession(sessionFormData).subscribe({
       next: (res) => {
         this.tstSrv.success('New Session Created');
         localStorage.setItem('session_id', res.session.session_id);
@@ -87,8 +98,14 @@ export class CreateComponent implements OnInit {
     });
   }
 
-  resetSessionForm() {
-    this.sessionForm.reset(this.initSessionForm().value);
+  resetSessionForm(section: '1' | '2' | '' = '') {
+    const val = this.initSessionForm();
+    if (section === '1') this.step1Form.reset(val[0].value);
+    else if (section === '2') this.step2Form.reset(val[1].value);
+    else {
+      this.step1Form.reset(val[0].value);
+      this.step2Form.reset(val[1].value);
+    }
   }
 
   openModal() {
